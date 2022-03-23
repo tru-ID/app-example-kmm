824f457b2cbebc430c696ed35f7cc9cc0adf0aac
Didem Yakici <didematilla@gmail.com>
2022-03-23 10:50:54 +0000

    iOS method check and print, logo and AppIcon added

--- PhoneCheckiOSApp/PhoneCheckiOSApp/ContentView.swift
+++ PhoneCheckiOSApp/PhoneCheckiOSApp/ContentView.swift
@@ -1,11 +1,15 @@
 import SwiftUI
 import shared
 
+
 struct ContentView: View {
 
 	var body: some View {
-		Text("Hello")
+		Text("Hello ")
+        var isReachable = Platform().isReachable()
+        let _ = print("isReachable called in iOS")
 	}
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
