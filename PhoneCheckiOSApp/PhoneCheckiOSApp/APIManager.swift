da35ce4f13d653afcfb6de76b25afa18bdd93c32
Didem Yakici <didematilla@gmail.com>
2022-04-06 21:29:42 +0100

    initial working swiftUI phoneCheck implementation

---
+++ PhoneCheckiOSApp/PhoneCheckiOSApp/APIManager.swift
@@ -0,0 +1,133 @@
+//
+//  APIManager.swift
+//  PhoneCheckiOSApp
+//
+//  Created by Didem Yakici on 05/04/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+final class APIManager {
+    
+    private let serverUrl = Bundle.main.object(forInfoDictionaryKey: "appServerUrl") as! String
+    enum CheckError: Error{
+        case badRequest
+        case internalError
+    }
+    
+    func postCheck(withPhoneNumber phone:String, completionHandler: @escaping(Result<Check,CheckError>)  -> Void) {
+        let json: [String:Any] = ["phone_number": phone]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        let config = URLSessionConfiguration.ephemeral
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForResource = 300
+        config.allowsExpensiveNetworkAccess = true
+        config.allowsCellularAccess = true
+        let session = URLSession(configuration: config)
+        print("serverUrl: \(serverUrl)")
+        let endPoint: String = serverUrl + "/check"
+        print("postCheck: \(endPoint)")
+        
+        let url = URL(string: endPoint)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.setValue("application/json", forHTTPHeaderField: "content-type")
+        print("request " + request.description)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error returning phone \(phone): \(error)")
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                print("response status code: \(httpResponse.statusCode)")
+                if (!(200...299).contains(httpResponse.statusCode)) {
+                    if ((400...499).contains(httpResponse.statusCode)) {
+                        completionHandler(.failure(.badRequest))
+                    }
+                    if (httpResponse.statusCode >= 500) {
+                        completionHandler(.failure(.internalError))
+                    }
+                    return
+                }
+            }
+            if let data = data,
+                let check = try? JSONDecoder().decode(Check.self, from: data) {
+                    completionHandler(.success(check))
+                }
+            }
+        task.resume()
+            
+        }
+        
+    func getCheckStatus(withCheckId id: String, completionHandler: @escaping (CheckStatus) -> Void) {
+        let config = URLSessionConfiguration.default
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForResource = 300
+        let session = URLSession(configuration: config)
+        let endPoint: String = serverUrl + "/check_status?check_id=\(id)"
+        print("getCheckStatus: \(endPoint)")
+        if let url = URL(string: endPoint) {
+            print(url)
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error returning id \(id): \(error)")
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                          print("Unexpected response status code: \(response)")
+                          return
+                      }
+                if let data = data,
+                   let check = try? JSONDecoder().decode(CheckStatus.self, from: data) {
+                    completionHandler(check)
+                }
+            }
+            task.resume()
+        } else {
+            print("error")
+        }
+    }
+    
+    struct Check: Codable {
+      let id: String
+      let url: String
+      
+      enum CodingKeys: String, CodingKey {
+        case id = "check_id"
+        case url = "check_url"
+      }
+      
+      init(id: String,
+           url: String) {
+        self.id = id
+        self.url = url
+      }
+    }
+    
+    struct CheckStatus: Codable {
+      let id: String
+      let match: Bool
+
+      enum CodingKeys: String, CodingKey {
+        case id = "check_id"
+        case match
+      }
+
+      init(id: String, match: Bool) {
+        self.id = id
+        self.match = match
+      }
+    }
+
+    
+    
+}
--- PhoneCheckiOSApp/PhoneCheckiOSApp/ContentView.swift
+++ PhoneCheckiOSApp/PhoneCheckiOSApp/ContentView.swift
@@ -8,86 +8,149 @@ struct ContentView: View {
     @State var termsAndConditionsSwitch: Bool = false
     @State var verifyButton: Bool = false
     @State var isLoading: Bool = false
-    var body: some View {
-        VStack {
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-            
-            Text("To experience tru.ID verification, please enter your phone number")
-                .font(.body)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            TextField("Phone number", text: $phoneNumber)
-                .textFieldStyle(DefaultTextFieldStyle())
-                //.keyboardType(.phonePad)
-                .multilineTextAlignment(.center)
-                .font(.title)
-            
-//            let _ = print(phoneNumber)
-            HStack {
-                Toggle("switch", isOn: $termsAndConditionsSwitch)
-                    .toggleStyle(SwitchToggleStyle(tint: .green))
-                    .labelsHidden()
-                Text("I agree with tru.ID terms and privacy policy")
-            }
-            .padding()
-            .multilineTextAlignment(.center)
+    @State var check: APIManager.Check?
+    @State var checkStatus: APIManager.CheckStatus?
 
-            Button ("Verify my phone number") {
-                //phonecheck to be performed
-                //progressView and result states to be managed
-                verifyButton.toggle()
-                isLoading.toggle()
-            }
-            .padding()
-            .font(.title2)
-            .background(Color.blue)
-            .foregroundColor(Color.white)
-            
-           
-            if self.isLoading {
-                ProgressView("")
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .font(.largeTitle)
-            }
-            if verifyButton {
-                Text("result")
-                    .font(.largeTitle)
-            }
-            
-            let platform = Platform()
-            
-            let _ = print("1 - App: checkUrlWithResponseBody will be called")
-            let _ = platform.checkUrlWithResponseBody(url: "http://www.cnn.com") { responseDict, error in
-                print("Last - App: checkUrlWithResponseBody:: Swift closure call with \(responseDict) - \(error)")
-            }
-            
-//            let _ = platform.checkWithTrace(url: "http://www.cnn.com") { traceInfo, error in
-//                print("checkWithTrace:: Swift closure call with \(traceInfo) - \(error)")
-//            }
-            
-//            do{
-//
-//            } catch {
-//
-//            }
-            //The problem is that the second parameter for the error only "catches" cancellation exceptions, all other errors thrown from the coroutine will need to be caught.
-//            let _ = platform.isReachable { reachabilityDetails, error in
-//                print("isReachable:: Swift closure call with \(reachabilityDetails) - \(error)")
-//            }
-//
-//
-//            let _ = platform.isReachableWithDataResidency(dataResidency: "EU") { reachabilityDetails, error in
-//                print("isReachableWithDataResidency:: Swift closure call with \(reachabilityDetails) - \(error)")
-//            }
-           
-            Spacer()
-            
-        }.padding()
+    let platform = Platform()
+    
+    var body: some View {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("To experience tru.ID verification, please enter your phone number")
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                TextField("Phone number", text: $phoneNumber)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                //.keyboardType(.phonePad)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                
+                //let _ = print(phoneNumber)
+                HStack {
+                    Toggle("switch", isOn: $termsAndConditionsSwitch)
+                        .toggleStyle(SwitchToggleStyle(tint: .green))
+                        .labelsHidden()
+                    Text("I agree with tru.ID terms and privacy policy")
+                }
+                .padding()
+                .multilineTextAlignment(.center)
+                
+                Button ("Verify my phone number") {
+                    //phonecheck to be performed
+                    //progressView and result states to be managed
+                    
+                    doPhoneCheck(phoneNumber: phoneNumber)
+                    let _ = print(phoneNumber)
+                    verifyButton.toggle()
+                    isLoading.toggle()
+                }
+                .padding()
+                .font(.title2)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                
+                
+                if self.isLoading {
+                    ProgressView("")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .font(.largeTitle)
+                }
+                if verifyButton {
+                    Text("result")
+                        .font(.largeTitle)
+                }
+                
+                //            DispatchQueue.main.async {
+                //                print("[isReachable] Starting")
+                //                self.platform.isReachable { reachabilityDetails, error in
+                //                    switch(reachabilityDetails) {
+                //                    case .success(let reachability): print(" reachability \(reachability)")
+                //                    case .failure(let error): print("reachability error \(error)")
+                //                    }
+                //                    print("[isReachable] Done")
+                //                }
+                //            }
+                //
+                
+                
+                //            let _ = print("1 - App: checkUrlWithResponseBody will be called")
+                //            let _ = platform.checkUrlWithResponseBody(url: url) { responseDict, error in
+                //                print("Last - App: checkUrlWithResponseBody:: Swift closure call with \(responseDict) - \(error)")
+                //            }
+                
+                //            let _ = platform.checkWithTrace(url: "http://www.cnn.com") { traceInfo, error in
+                //                print("checkWithTrace:: Swift closure call with \(traceInfo) - \(error)")
+                //            }
+                
+                //            do{
+                //
+                //            } catch {
+                //
+                //            }
+                //The problem is that the second parameter for the error only "catches" cancellation exceptions, all other errors thrown from the coroutine will need to be caught.
+                //            let _ = platform.isReachable { reachabilityDetails, error in
+                //                print("isReachable:: Swift closure call with \(reachabilityDetails) - \(error)")
+                //            }
+                //
+                //
+                //            let _ = platform.isReachableWithDataResidency(dataResidency: "EU") { reachabilityDetails, error in
+                //                print("isReachableWithDataResidency:: Swift closure call with \(reachabilityDetails) - \(error)")
+                //            }
+                
+                Spacer()
+                
+            }.padding()
     }
+    
+    func doPhoneCheck(phoneNumber: String) {
+        APIManager().postCheck(withPhoneNumber: phoneNumber) { (result) in
+            switch result {
+            case .success(let c):
+                DispatchQueue.main.async {
+                    self.check = c // Should any state variable concerning UI be modified in Main Thread/Queue?
+                    let _ = print("1 - App: checkUrlWithResponseBody will be called")
+                    self.platform.checkUrlWithResponseBody(url: self.check!.url) { body, error in
+                        print("Last - App: checkUrlWithResponseBody:: Swift closure call with \(body) - \(error)")
+                        APIManager().getCheckStatus(withCheckId: self.check!.id) { (s) in
+                            DispatchQueue.main.async {
+                                //Rollback the UI state with the updated results.
+                                verifyButton.toggle()
+                                isLoading.toggle()
+                                self.checkStatus = s
+                                if (s.match) {
+                                    print("\u{2705}")
+                                } else {
+                                    print("\u{274C}")
+                                }
+                            }
+                        }
+                    }
+                }
+            case .failure(let status):
+                DispatchQueue.main.async {
+                    //Rollback the UI state with the updated results.
+                    verifyButton.toggle()
+                    isLoading.toggle()
+                    if (status == APIManager.CheckError.badRequest) {
+                        print("\u{274C} wrong format")
+                    } else {
+                        print("\u{274C} error")
+                    }
+                }
+               
+            }
+            
+        }
+    }
+    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
