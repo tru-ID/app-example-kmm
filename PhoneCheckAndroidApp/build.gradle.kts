b8ba7b09ed3215dee5582a36c1d6f35954d4f4d6
Didem Yakici <didematilla@gmail.com>
2022-04-11 10:46:43 +0100

    temp solution for baseUrl as static variable within buildConfigField

--- PhoneCheckAndroidApp/build.gradle.kts
+++ PhoneCheckAndroidApp/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 //def props = new Properties()
 //file("tru.properties").withInputStream { props.load(it) }
 
+//val fis = java.io.FileInputStream("tru.properties")
+//val prop = Properties()
+//prop.load(fis)
+
 android {
     compileSdk = 30
     defaultConfig {
@@ -14,8 +18,7 @@ android {
         versionCode = 1
         versionName = "1.0"
         // Consult the README on instructions for setting up SERVER_BASE_URL
-//        buildConfigField("String", "SERVER_BASE_URL", props.getProperty("EXAMPLE_SERVER_BASE_URL"))
-
+        buildConfigField("String", "SERVER_BASE_URL", "\"https://74b3-2a00-23c7-8589-8d01-2d6f-82cd-46df-5b07.ngrok.io\"")
 //        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
         
     }
